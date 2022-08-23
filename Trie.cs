ccff545cf1f1d6e6cf2d6e136ac83fed197e9e9c
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2022-08-23 01:39:50 -0500

    Create Trie.cs

---
+++ Trie.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+public class Trie
+{
+	private readonly Node _root;
+
+	public Trie() //base constructor. Initializes root as a null parentless node.
+    {
+		_root = new Node("^", 0, null);
+    }
+
+	public Node Prefix(string stringIn)
+    {
+		Node currentNode = _root;
+		Node result = currentNode;
+		foreach(char c in stringIn)
+        {
+			currentNode = currentNode.FindChildNode(c);
+			if (currentNode == null)
+            {
+				break;
+            }
+			result = currentNode;
+        }
+		return result;
+    }
+
+	public bool Search(string queryString)
+    {
+		Node prefix = Prefix(queryString);
+		return (prefix.Depth == queryString.Length && prefix.FindChildNode('$')!=null);
+    }
+
+	public void Insert(string s)
+    {
+		Node prefix = Prefix(s);
+		Node current = prefix;
+		for (int i = current.Depth; i < s.Length; i++)
+        {
+			Node newNode = new Node(s[i], current.Depth+1, current);
+			current.Children.Add(newNode);
+			current = newNode;
+        }
+		current.Children.Add(new Node('$', current.Depth + 1, current);
+    }
+
+
+}
+
+public class Node
+{
+	public char Value { get; set; }
+	public List<Node> Children { get; set; }
+	public Node Parent { get; set; }
+	public int Depth { get; set; }
+
+	public Node(char value, int depth, Node parent)
+	{
+		Value = value;
+		Children = new List<Node>();
+		Depth = depth;
+		Parent = parent;
+
+	}
+
+	public bool IsLeaf()
+	{
+		return (Children.Count == 0);
+	}
+
+	public Node FindChildNode(char c)
+    {
+		foreach (Node child in Children)
+        {
+			if (child.Value == c)
+				return child;
+        }
+		return null;
+    }
+
+	public void DeleteChild(char c)
+    {
+		for(int i = 0; i < Children.Count; i++)
+        {
+			if (Children[i].Value == c)
+            {
+				Children.RemoveAt(i);
+            }
+        }
+    }
+}
